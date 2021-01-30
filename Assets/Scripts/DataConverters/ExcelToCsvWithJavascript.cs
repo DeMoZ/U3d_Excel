ea75f1c1e7fcbda12a6937c6cd240e2e8f085c56
Roman Kolychev <demozbox@gmail.com>
2021-01-30 22:22:32 +0300

    Excel reading for webgl build with javascript implemented.

---
+++ Assets/Scripts/DataConverters/ExcelToCsvWithJavascript.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
+
+namespace DataConverters
+{
+    public class ExcelToCsvWithJavascript
+    {
+#if (!UNITY_EDITOR && UNITY_WEBGL)
+#pragma warning disable CA2101
+        [DllImport("__Internal")]
+        private static extern string ExcelHexToCSV(string hex);
+
+        //  [DllImport("__Internal")]
+        //  private static extern void ExcelPathToCSV(string fileName);
+#pragma warning restore CA2101
+#endif
+
+
+        public string Convert(MemoryStream stream)
+        {
+            string hex = ByteArrayToString(stream.ToArray());
+            Debug.Log($"C# byte to hex: {hex}");
+
+            var rezult = string.Empty;
+#if (!UNITY_EDITOR && UNITY_WEBGL)
+#pragma warning disable CA2101
+            Debug.Log("C# ExcelHexToCSV =================================================");
+            rezult = ExcelHexToCSV(hex);
+            Debug.Log($"C# rezult received : \n {rezult}");
+
+            //  Debug.LogWarning("Read with javascript from disk ===================================");
+            //  string fileName = "TestFile.xlsx";
+            //  string filePath = string.Concat("StreamingAssets/", fileName);
+            //  ExcelPathToCSV(filePath);
+
+#pragma warning restore CA2101
+#endif
+
+            return rezult;
+        }
+
+        private string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/DataLoaders/IMemoryStreamLoader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace DataLoaders
+{
+    public interface IMemoryStreamLoader
+    {
+        void Load(string fileName, Action<MemoryStream> onCompleted, Action<string> onFailed);
+    }
+}
